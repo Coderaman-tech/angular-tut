6a903fef1cf445bf0041c7ab06e302ea1d7ab69f
Shrivastava <aman.h.shrivastava@accenture.com>
2026-02-09 02:25:38 +0530

    add wild-card-routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { GetSetFuncComponent } from './get-set-func/get-set-func.component';
 import { SignalsComponent } from './signals/signals.component';
 import { TodoComponent } from './todo/todo.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path:'counter',component:CounterAppComponent},
     {path:'get-set-func',component:GetSetFuncComponent},
     {path:'signals',component:SignalsComponent},
     {path:'todo',component:TodoComponent},
-    {path:'',component:HomeComponent}
+    {path:'',component:HomeComponent},
+    {path:"**",component:PageNotFoundComponent}
 
 ];
