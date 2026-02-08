8bca66dbb9d69512804db0dfa0dd7cb505725498
Shrivastava <aman.h.shrivastava@accenture.com>
2026-02-09 02:20:51 +0530

    add header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { CounterAppComponent } from './counter-app/counter-app.component';
 import { GetSetFuncComponent } from './get-set-func/get-set-func.component';
 import { SignalsComponent } from './signals/signals.component';
 import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:'counter',component:CounterAppComponent},
     {path:'get-set-func',component:GetSetFuncComponent},
     {path:'signals',component:SignalsComponent},
     {path:'todo',component:TodoComponent},
+    {path:'',component:HomeComponent}
 
 ];
