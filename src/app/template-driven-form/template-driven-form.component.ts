fa6b276793c1e79b3e3b3669ac688e49763a79a3
Aman Shrivastava <amankumarshrivastava835@gmail.com>
2026-03-12 23:54:59 +0530

    add validation

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule,NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
-  imports: [FormsModule],
+  imports: [FormsModule,NgIf],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.css'
 })
